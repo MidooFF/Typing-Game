f488ab81122ec0ee11fb1721fe1ffd854eb1a462
Mohammad <mohammad.sh12121@gmail.com>
2024-10-11 23:47:25 +0300

    added the main levle class and levels

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -9,7 +9,7 @@ function getWords(letters) {
         if (letters === 5)
             request.open("GET", "../../data/JSON-files/five-letters-words.json");
         if (letters === 6)
-            request.open("GET", "");
+            request.open("GET", "../../data/JSON-files/six-letters-words.json");
         request.onreadystatechange = function () {
             if (request.status === 200)
                 resolve(JSON.parse(request.responseText));
@@ -19,3 +19,35 @@ function getWords(letters) {
         request.send();
     });
 }
+class GetLevel {
+    constructor(name, letters, time) {
+        this.wordsArrs = [];
+        this.randomWordsArr = 0;
+        this.randomWordsInArr = 0;
+        this.wordsArr = [];
+        this.name = name;
+        this.letters = letters;
+        this.time = time;
+    }
+    getLevelWordsArrs(wordsCount) {
+        for (let i = 0; i < this.letters.length; i++) {
+            getWords(this.letters[i])
+                .then((resolveValue) => {
+                this.wordsArrs.push(resolveValue);
+                if (i === this.letters.length - 1) {
+                    for (let i = 0; i < wordsCount; i++) {
+                        this.randomWordsArr = Math.floor(Math.random() * this.wordsArrs.length);
+                        this.randomWordsInArr = Math.floor(Math.random() * this.wordsArrs[this.randomWordsArr].length);
+                        this.wordsArr.push(this.wordsArrs[this.randomWordsArr][this.randomWordsInArr]);
+                    }
+                }
+            })
+                .catch((err) => {
+                console.log(`Error: ${err}`);
+            });
+        }
+    }
+}
+let easy = new GetLevel("Easy", [3, 4], 8);
+let medium = new GetLevel("Medium", [4, 5], 6);
+let hard = new GetLevel("Hard", [6], 4);
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -12,7 +12,7 @@ function getWords(letters: number) {
             request.open("GET", "../../data/JSON-files/five-letters-words.json");
 
         if (letters === 6) 
-            request.open("GET", "")
+            request.open("GET", "../../data/JSON-files/six-letters-words.json");
 
         request.onreadystatechange = function() {
             if (request.status === 200)
@@ -24,3 +24,42 @@ function getWords(letters: number) {
         request.send();
     })
 }
+
+class GetLevel {
+    public name: string;
+    public letters: number[];
+    public time: number;
+    constructor(name: string, letters: number[], time: number) {
+        this.name = name;
+        this.letters = letters;
+        this.time = time;
+    }
+
+    protected wordsArrs: (string[])[] = [];
+    protected randomWordsArr: number = 0;
+    protected randomWordsInArr: number = 0;
+    public wordsArr: string[] = [];
+    getLevelWordsArrs(wordsCount: number): void {
+        for (let i = 0; i < this.letters.length; i++) {
+            getWords(this.letters[i])
+            .then((resolveValue) => {
+                this.wordsArrs.push(resolveValue as string[]);
+                if (i === this.letters.length - 1) {
+                    for (let i = 0; i < wordsCount; i++) {
+                        this.randomWordsArr = Math.floor(Math.random() * this.wordsArrs.length);
+                        this.randomWordsInArr = Math.floor(Math.random() * this.wordsArrs[this.randomWordsArr].length);
+                        this.wordsArr.push(this.wordsArrs[this.randomWordsArr][this.randomWordsInArr]);
+                    }
+                }
+            })
+            .catch((err) => {
+                console.log(`Error: ${err}`);
+            })
+        }
+    }
+}
+
+let easy = new GetLevel("Easy", [3, 4], 8);
+let medium = new GetLevel("Medium", [4, 5], 6);
+let hard = new GetLevel("Hard", [6], 4)
+
